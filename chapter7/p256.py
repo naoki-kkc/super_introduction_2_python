81a9644dd1dc4368bbdd8eead56e7a459e67fbfc
naoki-kkc <naoki2kuchi@gmail.com>
2022-06-19 17:53:53 +0900

    add chapter7 codes until page 276

--- chapter7/p256.py
+++ chapter7/p256.py
@@ -1,6 +1,7 @@
 # Jupyter notebookではうまく動かなかったのでpythonファイルで記述
 
 import tkinter as tk
+import tkinter.messagebox as msg
 
 def p256():
     base = tk.Tk()
@@ -86,6 +87,76 @@ def p264():
 def p264_call_method():
     print('p264_called!')
 
+def p265():
+    base = tk.Tk()
+    tk.Label(base, text='red', bg='red', width=20).pack()
+    tk.Label(base, text='blue', bg='blue', width=20).pack()
+    tk.Label(base, text='green', bg='green', width=20).pack()
+    base.mainloop()
+
+def p266():
+    base = tk.Tk()
+    select_dict = {0: 'hoge', 1:'fuga', 2:'foo', 3:'bar'}
+    check_dict  = {}
+
+    for i in range(len(select_dict)):
+        check_dict[i] = tk.BooleanVar()
+        tk.Checkbutton(base, variable=check_dict[i], text=select_dict[i]).pack()
+    
+    def buy():
+        for i in check_dict:
+            if check_dict[i].get():
+                print(select_dict[i])
+    
+    tk.Button(base, text='order', command=buy).pack()
+    base.mainloop()
+
+def p268():
+    base = tk.Tk()
+    radio_val = tk.IntVar()
+    radio_val.set(1)
+    radio_dict = {0:'hoge', 1:'fuga', 2:'foobar'}
+    tk.Radiobutton(base, text = radio_dict[0], variable=radio_val, value=0).pack()
+    tk.Radiobutton(base, text = radio_dict[1], variable=radio_val, value=1).pack()
+    tk.Radiobutton(base, text = radio_dict[2], variable=radio_val, value=2).pack()
+    def p268_btn():
+        val = radio_val.get()
+        print(radio_dict[val])
+    tk.Button(base, text='order', command=p268_btn).pack()
+    base.mainloop()
+
+def p270():
+    base = tk.Tk()
+    alert_response = msg.askyesno('alert_title', 'alert_message')
+    if alert_response:
+        print('ok')
+    else:
+        print('no')
+    base.mainloop()
+
+def p271():
+    base = tk.Tk()
+
+    str = tk.StringVar()
+    tk.Entry(base, textvariable=str).pack()
+    tk.Label(base, textvariable=str).pack()
+
+    base.mainloop()
+
+def p272():
+    base = tk.Tk()
+
+    menu   = tk.Menu(base)
+    f_menu = tk.Menu(menu)
+    f_menu.add_command(label='open file')
+    f_menu.add_separator()
+    f_menu.add_command(label='save file')
+    menu.add_cascade(label='file', menu=f_menu)
+
+    base.config(menu=menu)
+    base.mainloop()
+
+
 if __name__ == '__main__':
     # p256()
     # p258()
@@ -93,4 +164,10 @@ if __name__ == '__main__':
     # p261()
     # p262()
     # p263()
-    p264()
+    # p264()
+    # p265()
+    # p266()
+    # p268()
+    # p270()
+    # p271()
+    p272()
